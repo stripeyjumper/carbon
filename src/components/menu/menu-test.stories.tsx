54374990287ab277354af490a344e8a76322c3e3
DipperTheDan <daniel.dipper@sage.com>
2023-05-26 15:08:25 +0100

    docs(menu): add storybook actions story

--- src/components/menu/menu-test.stories.tsx
+++ src/components/menu/menu-test.stories.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { within, userEvent } from "@storybook/testing-library";
 import { action } from "@storybook/addon-actions";
 
+import { ComponentStory } from "@storybook/react";
 import { Menu } from ".";
 import { MenuType } from "./menu.context";
 import MenuItem from "./menu-item";
@@ -130,3 +132,31 @@ export const LongLabelsStory = () => {
 };
 
 LongLabelsStory.storyName = "submenus with long item labels";
+
+export const MenuExampleStory: ComponentStory<typeof Menu> = () => {
+  return (
+    <Menu>
+      <MenuItem submenu="Parent Menu A">
+        <MenuItem>Child A</MenuItem>
+        <MenuItem>Child B with very long label</MenuItem>
+        <MenuItem>Child C</MenuItem>
+      </MenuItem>
+      <MenuItem submenu="Parent Menu B with very long label">
+        <MenuItem>Child A</MenuItem>
+        <MenuItem>Child B</MenuItem>
+        <MenuItem>Child C</MenuItem>
+      </MenuItem>
+    </Menu>
+  );
+};
+
+export const TestStory = MenuExampleStory.bind({});
+TestStory.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  await userEvent.click(canvas.getByText("Parent Menu A"));
+
+  await userEvent.click(canvas.getByText("Child A"));
+};
+
+TestStory.storyName = "Storybook Actions Test";
